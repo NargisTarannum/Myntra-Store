b84810bfdb2b2b8ab23d2a8649db471fe6523d92
nargis <nargis.tarannum@cairsolutions.in>
2026-01-06 15:26:02 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: '/Myntra-Store/',
   plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
+    react(),
   ],
 })
