d95e0808a26e18d0d8ab1ff373ab0b8b111502ab
nargis <nargis.tarannum@cairsolutions.in>
2026-01-06 15:35:37 +0530

    added images folder and stored all images inside it

--- src/components/Home/HomePhotoShoot.jsx
+++ src/components/Home/HomePhotoShoot.jsx
@@ -3,11 +3,11 @@ const HomePhotoShoot = () => {
         <div  className='homePhotoshot'>
         <div className="photoshoot-container">
             <div className="model_photo_wrapper">
-                <img src="/boy_1.webp" alt="boy_1" className="model_photo_boy_1"/>
-                 <img src="/boy_2.webp" alt="boy_2" className="model_photo_boy_2"/>
+                <img src="images/boy_1.webp" alt="boy_1" className="model_photo_boy_1"/>
+                 <img src="images/boy_2.webp" alt="boy_2" className="model_photo_boy_2"/>
             </div>
              <div className="model_photo_wrapper">
-                <img src="/female.webp" alt="female" height="60" weight="60" className="model_photo_girl"/>
+                <img src="images/female.webp" alt="female" height="60" weight="60" className="model_photo_girl"/>
             </div>
         </div>
         </div>
